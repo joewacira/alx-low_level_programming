fec2eaf063256e69e59d85d18571d322c284fe22
joewacira <josephwmuthoni@gmail.com>
2023-10-05 14:24:10 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - entry point of this program
- * Return: 1 success running of the program
+ * Return 1 success running of the program
  */
 int main(void)
 {
-	fput("and that piece of art is useful\ - Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }

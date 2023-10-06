000d82521fb9899bc01b71810a673a4f4c0d5bbe
joewacira <josephwmuthoni@gmail.com>
2023-10-06 10:46:49 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - It indicates the main entry of a program.
+ *
+ * Return: 0 return value 0 if program runs successfully
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}

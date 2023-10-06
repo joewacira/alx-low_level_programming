320c7d5e57c5fc790c30208cec174de0bf875be9
joewacira <josephwmuthoni@gmail.com>
2023-10-06 10:16:28 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-char alphabet[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$";
+char alphabet[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int x;
 
 	for (x = 0; x < 52; x++)
 	{
-		putchar(alphabet[x]);
+	    putchar(alphabet[x]);
 	}
 	putchar('\n');
 

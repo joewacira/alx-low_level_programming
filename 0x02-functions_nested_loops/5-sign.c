46db35ea56cb349bf6ae7d6be383f75bd3f31f93
joewacira <josephwmuthoni@gmail.com>
2023-10-11 11:10:56 -0700

    print_sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,24 +1,26 @@
 #include "main.h"
 
 /**
- * print sign
+ * print_sign - prints sighn
+ * @n: numbers
  * Return: 1, 0 or -1
  */
 int print_sign(int n)
+
 {
 	if (n > 0)
 	{
 	_putchar(43);
 	return (1);
 	}
-       	else if (n < 0)
-	{	
+	else if (n < 0)
+	{
 	_putchar(45);
 	return (-1);
 	}
-       	else
+	else
 	{
 	_putchar(48);
 	return (0);
-        }
+	}
 }

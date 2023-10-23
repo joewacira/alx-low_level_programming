370a7828112a202689df83997b028a4e9367e834
joewacira <josephwmuthoni@gmail.com>
2023-10-23 08:54:17 -0700

    task 100

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,17 +1,14 @@
 #include "main.h"
 
 /**
- *   * set_string - main function of the program.
- *     *
- *       * @s: Function param
- *         *
- *           * @to: Function param
- *             *
- *               * Return: (0)
- *                 */
+ * set_string - main function of the program.
+ * @s: Function param
+ * @to: Function param
+ * Return: (0)
+ */
 void set_string(char **s, char *to)
 
 {
-		*s = to;
+	*s = to;
 
 }

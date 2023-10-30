6b50541f9ec69e7474d68ab025d7f3e38c663646
joewacira <josephwmuthoni@gmail.com>
2023-10-30 14:17:52 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,61 +1,52 @@
 #include "main.h"
+
 #include <stdlib.h>
 
 /**
- * str_len - Return the len string.
- * @str: input string.
- * Return: The len string.
- */
-int str_len(char *str)
-{
-	int len = 0;
-
-	while (str[len] != '\0')
-
-	{
-	len++;
-	}
-	return (len);
-}
-/**
- * str_copy - Copy a source string from source.
- * @src: source of the string.
- * @dest: The destination string.
- */
-void str_copy(char *src, char *dest)
-{
-	while (*src != '\0')
-	{
-	*dest = *src;
-	src++;
-	dest++;
-	}
-	*dest = '\0';
-}
-
-/**
- * str_concat - Concatenate 2 strings.
- * @s1: 1st string.
- * @s2: 2nd string.
- * Return: A newly allocated string of concatenated (s1) and (s2).
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
  */
 char *str_concat(char *s1, char *s2)
 {
+	char *conct;
+	int i, ci;
+
 	if (s1 == NULL)
 	s1 = "";
+
 	if (s2 == NULL)
 	s2 = "";
 
-	int len1 = str_len(s1);
-	int len2 = str_len(s2);
+	i = ci = 0;
 
-	char *concatenated = malloc(sizeof(char) * (len1 + len2 + 1));
+	while (s1[i] != '\0')
+	i++;
 
-	if (concatenated == NULL)
+	while (s2[ci] != '\0')
+	ci++;
+
+	conct = malloc(sizeof(char) * (i + ci + 1));
+
+	if (conct == NULL)
 	return (NULL);
 
-	str_copy(s1, concatenated);
-	str_copy(s2, concatenated + len1);
+	i = ci = 0;
 
-	return (concatenated);
+	while (s1[i] != '\0')
+	{
+	conct[i] = s1[i];
+	i++;
+	}
+
+	while (s2[ci] != '\0')
+	{
+	conct[i] = s2[ci];
+	i++, ci++;
+	}
+
+	conct[i] = '\0';
+
+	return (conct);
 }
